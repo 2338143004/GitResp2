62a07aa624f837ffa2ce70cfbfa2cb7cb33ccbae
zhangjiayuanss <932816537@qq.com>
2022-05-23 16:38:55 +0800

    向Student类中添加了set、get方法

--- TestModule01/src/com/msb/java/Student.java
+++ TestModule01/src/com/msb/java/Student.java
@@ -4,4 +4,28 @@ public class Student {
     private String name;
     private char sex;
     private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public char getSex() {
+        return sex;
+    }
+
+    public void setSex(char sex) {
+        this.sex = sex;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 }
